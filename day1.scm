267a543f983392c61f595a257b317f72e40ae0e3
Yovan Naumovski <yovan@gorski.stream>
2022-12-01 12:27:38 +0200

    day 1 part 1

---
+++ day1.scm
@@ -0,0 +1,29 @@
+(use-modules (ice-9 textual-ports)
+             (ice-9 rdelim))
+
+(define (parse-input-file filename)
+  (let ((port (open-input-file filename)))
+    (let parse-line ((groups '())
+                     (current-group '()))
+      (let ((current-line (read-line port)))
+        (cond
+         ((eof-object? current-line) groups)
+         ((string=? "" current-line) (parse-line (cons current-group groups) '()))
+         (else (parse-line groups (cons current-line current-group))))))))
+
+(define (strs->numbers strs)
+  (map string->number strs))
+
+(define (list-sum list)
+  (apply + list))
+
+(define (sum-entry entry)
+  (list-sum (strs->numbers entry)))
+
+(define (part1 parsed-input)
+  (let ((sums (map (lambda (sublist) (sum-entry sublist)) parsed-input)))
+    (car (sort sums >))))
+
+(display "Part 1: ")
+(display (part1 (parse-input-file "inputs/day1")))
+(newline)
