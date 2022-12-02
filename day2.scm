e9c256627b826972c84ab3d1e3b2b2ccef011c7d
Yovan Naumovski <yovan@gorski.stream>
2022-12-02 20:38:49 +0200

    add day 2 part 2

--- day2.scm
+++ day2.scm
@@ -1,7 +1,7 @@
 (use-modules (ice-9 textual-ports)
              (ice-9 rdelim))
 
-(define (parse-input-file filename)
+(define (day2 filename player-move-fn)
   (let ((port (open-input-file filename)))
     (let parse-line ((total 0))
       (let ((current-line (read-line port)))
@@ -9,8 +9,11 @@
             total
             (letrec ((round-moves (string->list current-line))
                      (enemy-move (car round-moves))
-                     (player-move (caddr round-moves)))
-              (parse-line (+ total (+ (move-value player-move) (outcome enemy-move player-move))))))))))
+                     (player-move (player-move-fn enemy-move (caddr round-moves))))
+              (parse-line (+ total
+                             (+
+                              (move-value player-move)
+                              (outcome enemy-move player-move))))))))))
 
 (define (move-value move-char)
   (cond
@@ -33,6 +36,21 @@
        ((eq? diff 2) 6)))))
 
 (display "day 2 part 1: ")
-(display (parse-input-file "inputs/day2"))
+(display (day2 "inputs/day2" (lambda (_ player-move) player-move)))
 (newline)
 
+(define (part2-player-move enemy-move player-move)
+  (letrec ((enemy-move-value (move-value enemy-move))
+           (player-move-value (move-value player-move))
+
+           (new-player-move-value (modulo
+                                   (- enemy-move-value (- player-move-value)) ;lol
+                                   3)))
+    (cond
+     ((eq? new-player-move-value 0) #\X)
+     ((eq? new-player-move-value 1) #\Y)
+     ((eq? new-player-move-value 2) #\Z))))
+
+(display "day 2 part 2: ")
+(display (day2 "inputs/day2" (lambda (enemy-move player-move) (part2-player-move enemy-move player-move))))
+(newline)
